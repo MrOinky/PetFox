1d3754acf4f2414feff2860cdc2fd78668654301
MrOinky <61294271+MrOinky@users.noreply.github.com>
2020-03-04 22:36:29 +0000

    add bot.py, some cleanup
    bottoken.txt is now a required file that the user must input, cleanup unneeded items in .gitignore, update README and add some very very basic core functionality.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 # Notice
  This is a WIP standalone bot and is likely not suited for personal use as of now, however it will become easier to use personally over time.
  (This is also atm just the repositry setup so note that it doesn't even exist yet, it needs to be entirely converted from my previous cog on RedBot first.)
+
+# Setup (WIP)
+ Before you can even run PetFox locally you need to create a file inside the downloaded repository called bottoken.txt and then put in a valid bot account token you can use.
+ You also require discord.py to be installed, which you can find the instructions for here: https://discordpy.readthedocs.io/en/latest/intro.html
---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import sys, traceback, logging
+import time, os, json
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=logging.INFO)
+bot = commands.Bot(command_prefix="-", activity=discord.Game("Booting PetFox!"))
+logging.info(f"Booting into PetFox...")
+
+@bot.event
+async def on_ready():
+    logging.info(f"Booted successfully as: {bot.user.name} with id {bot.user.id}")
+    logging.info(f"Thank you for using PetFox.")
+    await bot.change_presence(activity=discord.Game("with foxes!"))
+
+@bot.command(name="shutdown", aliases = ["close"])
+async def shutdown(ctx):
+    await ctx.send("Closing PetFox v0.0.1 (the nothingness) :wave:")
+    logging.info(f"Shutting down PetFoxBot.")
+    await bot.close()
+    logging.info(f"Done.")
+    exit()
+
+try:
+    token = open("bottoken.txt", "r+").read()
+except:
+    logging.error(f"failed to find a token for the bot to login with, can not continue. Please insert a file into the PetFox directory called bottoken.txt with a valid bot token")
+
+bot.run(token, bot=True, reconnect=True)
